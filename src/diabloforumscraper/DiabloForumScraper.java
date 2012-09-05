8487f32ffe215f0973fc1962fd741a2e8a303ff6
Josh <jwalto@DeathstarDevelopmentPlatform.(none)>
2012-09-04 22:45:22 -0600

    loop over topic pages logic fix

--- src/diabloforumscraper/DiabloForumScraper.java
+++ src/diabloforumscraper/DiabloForumScraper.java
@@ -25,119 +25,119 @@ import org.jsoup.select.Elements;
 public class DiabloForumScraper {
 
     //private static Logger log = Logger.getLogger(DiabloForumScraper.class);
-    
     private static final String DIABLO_ROOT = "http://us.battle.net/d3/en/";
-    private static final String GENERAL_DISCUSSION_ROOT = DIABLO_ROOT+"forum/3354739/";
-    private static final String FORUM_TOPIC_ROOT = DIABLO_ROOT+"forum/topic/";
+    private static final String GENERAL_DISCUSSION_ROOT = DIABLO_ROOT + "forum/3354739/";
+    private static final String FORUM_TOPIC_ROOT = DIABLO_ROOT + "forum/topic/";
     private static final String USER_AGENT = "Mozilla";
-    
     private DiabloData database;
-    
-    public DiabloForumScraper(DiabloData database){
+
+    public DiabloForumScraper(DiabloData database) {
         this.database = database;
     }
-    
-    public Document getDocument(String url){
+
+    public Document getDocument(String url) {
         Document doc = null;
         Connection connection = Jsoup.connect(url);
         connection.userAgent(USER_AGENT);
         try {
             doc = connection.get();
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         return doc;
     }
-    
-    public List<String> getForumTopicUrls(Document doc){
+
+    public List<String> getForumTopicUrls(Document doc) {
         List<String> topicIds = new ArrayList<String>();
         Elements forumTopicRows = doc.select("tbody tr");
-        System.out.println("Found "+forumTopicRows.size()+" topics");
-        for(Element forumTopicRow : forumTopicRows){
-            System.out.println("row html id = "+forumTopicRow.id());
+        System.out.println("Found " + forumTopicRows.size() + " topics");
+        for (Element forumTopicRow : forumTopicRows) {
+            System.out.println("row html id = " + forumTopicRow.id());
             String topicId = forumTopicRow.id().replace("postRow", "");
             topicIds.add(topicId);
-            System.out.println("topicId = "+topicId);
-            
+            System.out.println("topicId = " + topicId);
+
         }
-        
+
         return topicIds;
     }
-    
-    private String getNextPageLink(Document doc){
+
+    private String getNextPageLink(Document doc) {
         Elements nextPageSpans = doc.select("li.cap-item a span");
-        System.out.println("Found "+nextPageSpans.size()+" next page spans");
+        System.out.println("Found " + nextPageSpans.size() + " next page spans");
         String nextPageLink = null;
-        for(Element nextPageSpan : nextPageSpans){
+        for (Element nextPageSpan : nextPageSpans) {
             String spanText = nextPageSpan.text();
-            System.out.println("Span text = "+spanText);
-            if(spanText.equalsIgnoreCase("Next")){
+            System.out.println("Span text = " + spanText);
+            if (spanText.equalsIgnoreCase("Next")) {
                 //get parent which is the link tag
                 Element parent = nextPageSpan.parent();
                 nextPageLink = parent.attr("href");
-                System.out.println("Next page link = "+nextPageLink);
+                System.out.println("Next page link = " + nextPageLink);
                 break;
             }
         }
-        
+
         return nextPageLink;
     }
-    
-    private TopicScraperResult processTopic(String topicId, String pageLink){
+
+    private TopicScraperResult processTopic(String topicId, String pageLink) {
         List<String> profiles = new ArrayList<String>();
-        String topicPage = FORUM_TOPIC_ROOT+topicId;
-        if(pageLink != null){
-            topicPage+=pageLink;
+        String topicPage = FORUM_TOPIC_ROOT + topicId;
+        if (pageLink != null) {
+            topicPage += pageLink;
         }
-        System.out.println("Retrieving topic page: "+topicPage);
+        System.out.println("Retrieving topic page: " + topicPage);
         Document topicDocument = getDocument(topicPage);
         Elements profileLinks = topicDocument.select(".view-d3-profile");
-        System.out.println("Found "+profileLinks.size()+" profile links");
-        for(Element profileLink : profileLinks){
-           String link = profileLink.attr("href") ;
-           String profile = link.replace("/d3/en/profile/", "");
-           //remove ending forward slash
-           profile = profile.substring(0, profile.length()-1);
-           System.out.println("Found profile: "+profile);
-           profiles.add(profile);
+        System.out.println("Found " + profileLinks.size() + " profile links");
+        for (Element profileLink : profileLinks) {
+            String link = profileLink.attr("href");
+            String profile = link.replace("/d3/en/profile/", "");
+            //remove ending forward slash
+            profile = profile.substring(0, profile.length() - 1);
+            System.out.println("Found profile: " + profile);
+            profiles.add(profile);
         }
-        
+
         String nextPageLink = getNextPageLink(topicDocument);
-        
-        TopicScraperResult result= new TopicScraperResult(profiles, nextPageLink);
-        
+
+        TopicScraperResult result = new TopicScraperResult(profiles, nextPageLink);
+
         return result;
     }
-    
+
     /*
      * process a forum page that has links to topics like page: http://us.battle.net/d3/en/forum/3354739/
      */
-    public void processTopicListPage(Document topicListDoc){
+    public void processTopicListPage(Document topicListDoc) {
         List<String> topicIds = getForumTopicUrls(topicListDoc);
         String nextPageLink = null;
-        
-        for(String topicId : topicIds){
-            
+
+        for (String topicId : topicIds) {
+
             TopicScraperResult result = processTopic(topicId, nextPageLink);
             nextPageLink = result.nextPageLink;
             processProfiles(result.profiles);
-            while(nextPageLink != null){
-                processTopic(topicId, nextPageLink);
+            while (nextPageLink != null) {
+                result = processTopic(topicId, nextPageLink);
+                nextPageLink = result.nextPageLink;
+                processProfiles(result.profiles);
             }
         }
-        
+
         String nextPage = getNextPageLink(topicListDoc);
         int topicPagesProcessed = 1;
-        while(nextPage != null && topicPagesProcessed < 5){
-            Document nextPageDoc = getDocument(GENERAL_DISCUSSION_ROOT+nextPage);
+        while (nextPage != null && topicPagesProcessed < 5) {
+            Document nextPageDoc = getDocument(GENERAL_DISCUSSION_ROOT + nextPage);
             processTopicListPage(nextPageDoc);
-            
+
             topicPagesProcessed++;
         }
     }
-    
-    public void processProfiles(List<String> profiles){
+
+    public void processProfiles(List<String> profiles) {
         database.insertProfiles(profiles);
     }
 
@@ -148,16 +148,17 @@ public class DiabloForumScraper {
         DiabloData database = new DiabloData();
         DiabloForumScraper scraper = new DiabloForumScraper(database);
         Document doc = scraper.getDocument(GENERAL_DISCUSSION_ROOT);
-        
+
         scraper.processTopicListPage(doc);
-        
+
     }
-    
+
     private class TopicScraperResult {
+
         public List<String> profiles;
         public String nextPageLink;
-        
-        public TopicScraperResult(List<String> profiles, String nextPageLink){
+
+        public TopicScraperResult(List<String> profiles, String nextPageLink) {
             this.profiles = profiles;
             this.nextPageLink = nextPageLink;
         }
